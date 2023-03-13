b021f104c820d4c7dd97bdf66b11dbe307718f0e
MagdaLM <127141836+MagdaLM@users.noreply.github.com>
2023-03-13 15:39:54 +0100

    Add files via upload

---
+++ EXERCISE 2_3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 1*1 + 3*3* + 5*5 . . .+2021*2021
+# n*n+(n+2)*(n+2)+...
+
+# Function to calculate sum of series.
+def sumOfSeries(n):
+	sum = 0;
+	i = 1;
+	while i<=n:
+		sum = sum + i * i
+		i = i + 2
+	return sum
+
+# Driver code
+n = 2021
+print(sumOfSeries(n))
+
+
